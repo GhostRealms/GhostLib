9c9bbf4d187c4b9d3068ed27499065d12c65c380
River Marmorstein <river@rmarmorstein.com>
2015-02-26 04:44:58 -0500

    init

---
+++ src/main/java/net/ghostrealms/lib/UUIDLib.java
@@ -0,0 +1,22 @@
+package net.ghostrealms.lib;
+
+import java.util.UUID;
+
+/**
+ * Created by River on 2/26/2015.
+ * UUID Getter and Converter Library for Bukkit*
+ */ 
+
+public class UUIDLib {
+    
+    private UUIDLib() {}
+    
+    public static UUID getId(String user) {
+        return null;
+    }
+    
+    public static String getName(UUID id) {
+
+    }
+    
+}
